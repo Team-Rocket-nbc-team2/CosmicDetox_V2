3cc7023d2017a248a99028aed8ac6fc5b14acc56
ImGaram <gim78014@gmail.com>
2024-10-29 20:54:35 +0900

    add: cosmic detox 소셜 로그인 버튼 구현

--- app/src/main/java/com/rocket/cosmicdetox_v2/component/button/CosmicDetoxButton.kt
+++ app/src/main/java/com/rocket/cosmicdetox_v2/component/button/CosmicDetoxButton.kt
@@ -1,13 +1,17 @@
 package com.rocket.cosmicdetox_v2.component.button
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -17,6 +21,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.rocket.cosmicdetox_v2.R
 import com.rocket.cosmicdetox_v2.ui.theme.BackgroundLight
 import com.rocket.cosmicdetox_v2.ui.theme.BlueGrey
 import com.rocket.cosmicdetox_v2.ui.theme.Primary
@@ -122,6 +127,39 @@ fun CosmicDetoxBorderButton(
 }
 
 /**
+ * 소셜 로그인 전용 버튼
+ *
+ * - [onClick]: 버튼 클릭 시 이벤트 처리
+ * - [modifier]: 버튼의 크기, 추가 속성을 적용
+ * - [type]: 로그인할 sns 종류 선택([SocialSignInType] 사용)
+ */
+@Composable
+fun CosmicDetoxSocialSignInButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    type: SocialSignInType,
+) {
+    val containerColor = if (type == SocialSignInType.KAKAO) Color(0xFFFAE100) else White
+    val icon = when (type) {
+        SocialSignInType.KAKAO -> R.drawable.ic_kakao
+        SocialSignInType.GOOGLE -> R.drawable.ic_google
+        SocialSignInType.TWITTER_X -> R.drawable.ic_twitter_x
+    }
+
+    IconButton(
+        onClick = onClick,
+        modifier = modifier.size(48.dp),
+        colors = IconButtonDefaults.iconButtonColors(containerColor = containerColor)
+    ) {
+        Image(
+            painter = painterResource(id = icon),
+            contentDescription = "social sign in",
+            modifier = Modifier.size(28.dp)
+        )
+    }
+}
+
+/**
  * 버튼 기능 테스트하는 함수
  */
 @Preview
@@ -136,6 +174,19 @@ private fun CosmicDetoxButtonPreview() {
         CosmicDetoxBorderButton(
             onClick = {},
             text = "text border"
+        )
+
+        CosmicDetoxSocialSignInButton(
+            onClick = {},
+            type = SocialSignInType.KAKAO
+        )
+        CosmicDetoxSocialSignInButton(
+            onClick = {},
+            type = SocialSignInType.GOOGLE
+        )
+        CosmicDetoxSocialSignInButton(
+            onClick = {},
+            type = SocialSignInType.TWITTER_X
         )
     }
 }
