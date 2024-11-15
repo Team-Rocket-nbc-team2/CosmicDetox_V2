bb4c0cbf910e1f906090c169e248291ce70bf7a4
ImGaram <gim78014@gmail.com>
2024-11-15 17:35:21 +0900

    remove: cosmic detox app checkbox item 불필요한 import 삭제

--- app/src/main/java/com/rocket/cosmicdetox_v2/component/checkbox/CosmicDetoxCheckBox.kt
+++ app/src/main/java/com/rocket/cosmicdetox_v2/component/checkbox/CosmicDetoxCheckBox.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Card
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
