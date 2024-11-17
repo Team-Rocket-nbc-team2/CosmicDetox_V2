a1c1bd5147ca5643a17fcd38c989369cefc00335
ImGaram <gim78014@gmail.com>
2024-11-17 18:17:19 +0900

    add: cosmic detox 랭킹 item type 생성

---
+++ app/src/main/java/com/rocket/cosmicdetox_v2/component/item/RankingItemType.kt
@@ -0,0 +1,12 @@
+package com.rocket.cosmicdetox_v2.component.item
+
+/**
+ * 랭킹 item의 타입 지정
+ *
+ * @property MY_RANK 나의 랭킹 정보
+ * @property OTHER_RANK 내가 아닌 다른 사용자 정보
+ */
+enum class RankingItemType {
+    MY_RANK,
+    OTHER_RANK
+}
